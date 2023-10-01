40616fbd75f7c1c53a0c50b86c97d3ad3ed5ed96
leander006 <leanderdsilva06@gmail.com>
2023-10-01 16:14:57 +0530

    IIFE

---
+++ Advanced js/IIFE.js
@@ -0,0 +1,10 @@
+(function name() {
+  console.log("leander");
+})();
+// name IIFE
+
+(() => {
+  console.log("leanderss");
+})(); // unnamed IIFE
+
+// Semicol is needed to tell where scope gets over
