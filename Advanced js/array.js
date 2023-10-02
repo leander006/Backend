74eb8ab6febdbefb3cfe41e259dafbc66fe1abe1
leander006 <leanderdsilva06@gmail.com>
2023-10-02 17:49:30 +0530

    Dom manipulations

---
+++ Advanced js/array.js
@@ -0,0 +1,20 @@
+const arr = ["leander", "knock", "shreya", "riya", "aadil", "shiv"];
+// const obj = {
+//   id1: "leander",
+//   id2: "knock",
+//   id3: "shreya",
+//   id4: "riya",
+//   id5: "aadil",
+//   id6: "shiv",
+// };
+arr.forEach((r, index) => {
+  console.log("each", r, "index", index);
+});
+
+// for (const k in arr) {
+//   console.log("in", k);
+// }
+
+// for (const i of arr) {
+//   console.log("of", i);
+// }
